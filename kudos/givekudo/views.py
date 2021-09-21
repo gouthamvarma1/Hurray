2e867722e60c8db4b89fbb87959bd4ff890aa3ee
MrudulaMadhavan <mrudula.madhavan@target.com>
2021-09-21 20:51:15 +0530

    added notification

--- kudos/givekudo/views.py
+++ kudos/givekudo/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import KudoForm
@@ -7,6 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
 from datetime import datetime, timedelta
 from .models import Kudo
+
 
 # Create your views here.
 
@@ -36,6 +38,15 @@ def givekudo(request):
                     kudo_details=Kudo.objects.create(from_user=from_user, to_user=to_user, content=form.data.get("message"), kudo_count=form.data.get("kudo_count"))
                     kudo_details.save()
                     messages.success(request, 'Thank you for appreciating. Kudo Given!')
+                    r = requests.post('http://127.0.0.1:8000/api/students/sendEmail', data=
+                    {
+                        "email":  to_user.email,
+                        "status": 1
+                    })
+                    if r.status_code == 200:
+                        return HttpResponse('Yay, notified the receiver')
+                    return HttpResponse('Sorry, Could not notify the receiver')
+
         context = {'form': form}
         return render(request, 'givekudo/kudo.html', context)
     return render(request, 'givekudo/home.html')
@@ -48,10 +59,9 @@ def dashboard(request):
         start=today - timedelta(days=today.weekday())
         end=start + timedelta(days=6)
         kudo_data=Kudo.objects.filter(to_user=to_user).exclude(kudo_date__lt=start).filter(kudo_date__lt=end+timedelta(days=1))
-        dashboard_data=[{'from_user':kudo.from_user.username, 
-                         'kudo_count': kudo.kudo_count, 
+        dashboard_data=[{'from_user':kudo.from_user.username,
+                         'kudo_count': kudo.kudo_count,
                          'date_posted': str(kudo.kudo_date)} for kudo in kudo_data]
         context = {'dashboard': dashboard_data}
         return render(request, 'givekudo/dashboard.html', context)
     return render(request, 'givekudo/home.html')
-
